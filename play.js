54273925f4b4aefbf93d7b390a8c5c1b8e69bd0c
shihabfaruk <sfaruk3131p@gmail.com>
2026-02-26 17:10:59 -0500

    Complete UI and App

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 humanScore = 0;
 computerScore = 0;
 
+let options = document.querySelectorAll('.choice');
+let textArea = document.querySelector('.result');
+let pScore = document.querySelector('#pScore');
+let cScore = document.querySelector('#cScore');
+
+options.forEach((option) => {
+    option.addEventListener('click',() => {
+        if (humanScore < 5 && computerScore < 5) {
+            playRound(option.textContent, getComputerChoice());
+        }
+
+        if (humanScore === 5) {
+            textArea.textContent = "You won!";
+        } else if (computerScore === 5) {
+            textArea.textContent = "You Lost!";
+        }
+        
+    });
+});
+
 function getComputerChoice() {
     let min = 1;
     let max = 3;
@@ -26,39 +46,45 @@ function playRound(humanChoice, computerChoice) {
 
         if (humanChoice === computerChoice) {
             msg = "It's a draw!";
-            //console.log("It's a draw!");
+            console.log("It's a draw!");
         } else if (humanChoice === 'rock') {
             if (computerChoice === 'scissors') {
                 msg = "You Win! Rock beats Scissors";
-                //console.log("You Win! Rock beats Scissors");
+                console.log("You Win! Rock beats Scissors");
                 humanScore++;
+                pScore.textContent = humanScore;
             } else {
                 msg = "You Lose! Scissors beats Rock";
-                //console.log("You Lose! Scissors beats Rock");
+                console.log("You Lose! Scissors beats Rock");
                 computerScore++;
+                cScore.textContent = computerScore;
             }
         } else if (humanChoice === 'paper') {
             if (computerChoice === 'rock') {
                 msg = "You Win! Paper beats Rock";
-                //console.log("You Win! Paper beats Rock");
+                console.log("You Win! Paper beats Rock");
                 humanScore++;
+                pScore.textContent = humanScore;
             }  else {
                 msg = "You Lose! Scissors beats Paper";
-                //console.log("You Lose! Scissors beats Paper");
+                console.log("You Lose! Scissors beats Paper");
                 computerScore++;
+                cScore.textContent = computerScore;
             }
         } else if (humanChoice === 'scissors') {
             if (computerChoice === 'paper') {
                 msg = "You win! Scissors beats Paper";
-                //console.log("You win! Scissors beats Paper");
+                console.log("You win! Scissors beats Paper");
                 humanScore++;
+                pScore.textContent = humanScore;
             } else {
                 msg = "You Lose! Rock beats Scissors";
-                //console.log("You Lose! Rock beats Scissors");
+                console.log("You Lose! Rock beats Scissors");
                 computerScore++;
+                cScore.textContent = computerScore;
             }
         }
-        return msg;
+        textArea.textContent = msg;
     }
 
 /** 
@@ -77,8 +103,8 @@ function playGame() {
         console.log("Best of 5 is a draw!");
     }
     console.log(`Computer: ${computerScore} Player: ${humanScore}`);
-}*/
-
+}**/
+    
 
 //playGame();
 
